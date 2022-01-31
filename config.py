9cae1cd33d9ee1c4510006fb171689fe10ca37a3
Gold937 <92666428+Gold937@users.noreply.github.com>
2022-01-31 09:23:13 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+# Конфиг
+command_prefixes = ['.','!','/']
+
+ghoul_table_command = 'ghoul-c'
+end_message = 'Создал Gold' # Сообщение после конца цикла, если не нужно - оставляем пустым
+messages_per_second = 7 # Для ghoul_spam
+sleep_time_ghoul = 0.03 # Для ghoul_table 0.0825
+
+# Линии не трогать
+
+api_id = 1016382
+api_hash = 'c27834e5683d50a9bacf835a95ec4763'
+
+# Линии не трогать
---
+++ index.py
@@ -0,0 +1,33 @@
+from config import *
+from time import sleep
+from pyrogram import Client, filters, sync
+from pyrogram.errors import FloodWait
+
+client = Client('ghoul-session', api_id, api_hash)
+
+client.start()
+
+client.stop()
+
+print('Бот запущен, теперь ты - Гуль')
+
+
+@client.on_message(filters.regex('ZXC|zxc') & filters.me)
+def ghoul_spam_handler(client, message):
+
+i = 1000 
+
+while i > 0:
+        try:
+             client.send_message(message.chat.id, f'{i} - 7 = {i-7}')
+        except FloodWait as e:
+            sleep(e.x)            
+            
+         i -= 7   
+         sleep(1/messages_per_second)
+         
+       if(end_message != ''):
+        client.send_message(message.chat.id, end_message)
+  
+
+client.run()
