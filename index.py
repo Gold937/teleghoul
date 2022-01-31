7c625f6835698d3fbfc9603a327e680c28cc629e
Gold937 <92666428+Gold937@users.noreply.github.com>
2022-01-31 10:26:16 +0200

    Update index.py

--- index.py
+++ index.py
@@ -14,17 +14,15 @@ print('Бот запущен, теперь ты - Гуль')
 
 @client.on_message(filters.regex('ZXC|zxc') & filters.me)
 def ghoul_spam_handler(client, message):
-
-i = 1000 
-
-while i > 0:
-        try:
-             client.send_message(message.chat.id, f'{i} - 7 = {i-7}')
-        except FloodWait as e:
-            sleep(e.x)            
+        i = 1000 
+        while i > 0:
+                try:
+                     client.send_message(message.chat.id, f'{i} - 7 = {i-7}')
+                 except FloodWait as e:
+                          sleep(e.x)            
             
-         i -= 7   
-         sleep(1/messages_per_second)
+                  i -= 7   
+                  sleep(1/messages_per_second)
          
        if(end_message != ''):
         client.send_message(message.chat.id, end_message)
